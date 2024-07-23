b7c5b40972c82c8cee7bdcacc47f574f60387986
Sergio_Sena <senanetworker@gmail.com>
2024-07-23 18:18:20 -0300

    add header,footer,backgroud

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [SharedModule, HeaderComponent, MatTabsModule],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
+export class LandingPageComponent {}
 
-}
